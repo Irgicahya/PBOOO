f67669765418df7b1e4faaaf186ada378cd5fc33
Irgicahya <142205032+Irgicahya@users.noreply.github.com>
2023-11-21 09:32:01 +0700

    Delete bin directory

--- bin/dart_application_1.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
-import 'package:dart_application_1/hewan.dart';
-void main(List<String> arguments) {
-  Hewan hewan = Hewan();
-  
-  hewan.tampil();
-}
