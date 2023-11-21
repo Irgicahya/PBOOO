a334d61fb1e984f7e0f73df299c870c5fa1aff6a
Irgicahya <142205032+Irgicahya@users.noreply.github.com>
2023-11-21 09:32:28 +0700

    Add files via upload

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,9 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/hewan.dart';
+void main(List<String> arguments) {
+  Hewan hewan = Hewan();
+  hewan.nama = "burung";
+  hewan.habitat = "Udara";
+  hewan.jumlahmata = 2;
+  hewan.tampil();
+}
